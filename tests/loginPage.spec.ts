dfad0d1177fd432abf235731a6aacf88afb5219f
KristiyanDraganov <kristiandraganov@gmail.com>
2025-03-29 12:00:02 +0200

    Added unsuccessful login tests

--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -1,8 +1,9 @@
 import {test, expect} from '@playwright/test';
 
 
-test('Login to the site', async({ page }) => {
+test('Successful login to the site', async({ page }) => {
   await page.goto('https://play1.automationcamp.ir/login.html');
+  await expect(page).toHaveTitle('Login');
 
   const username = page.locator('#user');
   const password = page.locator('#password');
@@ -13,4 +14,38 @@ test('Login to the site', async({ page }) => {
   await loginBtn.click();
 
   await expect(page).toHaveTitle('Order Pizza');
+})
+
+
+test('Unsuccessful login - wrong username', async({ page }) => {
+  await page.goto('https://play1.automationcamp.ir/login.html');
+  await expect(page).toHaveTitle('Login');
+
+  const username = page.locator('#user');
+  const password = page.locator('#password');
+  const loginBtn = page.locator('#login');
+  const validatioMsg = page.locator('#message');
+
+  await username.fill('123');
+  await password.fill('admin');
+  await loginBtn.click();
+
+  await expect(validatioMsg).toBeVisible();
+})
+
+
+test('Unsuccessful login - wrong password', async({ page }) => {
+  await page.goto('https://play1.automationcamp.ir/login.html');
+  await expect(page).toHaveTitle('Login');
+
+  const username = page.locator('#user');
+  const password = page.locator('#password');
+  const loginBtn = page.locator('#login');
+  const validatioMsg = page.locator('#message');
+
+  await username.fill('admin');
+  await password.fill('123');
+  await loginBtn.click();
+
+  await expect(validatioMsg).toBeVisible();
 })
