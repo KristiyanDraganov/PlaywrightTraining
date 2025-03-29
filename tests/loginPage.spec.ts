5027cb4cbdb8bb47afe07c1245673fce64337b89
KristiyanDraganov <kristiandraganov@gmail.com>
2025-03-29 11:01:39 +0200

    Created login test

---
+++ tests/loginPage.spec.ts
@@ -0,0 +1,17 @@
+import {test, expect} from '@playwright/test';
+
+
+test('Login to the site', async({ page }) => {
+  await page.goto('https://play1.automationcamp.ir/login.html');
+
+  const username = page.locator('#user');
+  const password = page.locator('#password');
+  const loginBtn = page.locator('#login');
+
+  await page.pause();
+  await username.fill('admin');
+  await password.fill('admin');
+  await loginBtn.click();
+
+  await expect(page).toHaveTitle('Order Pizza');
+})
